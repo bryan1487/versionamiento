1ae0155fb3ea9b1e37890b601b33eb977f5ef019
bryan1487 <bryan.solorzano.sep@gmail.com>
2022-08-18 00:21:10 -0400

    segunda version

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def ping():
     return jsonify({"message": "pong!!"})
 
+@app.route('/products', methods=['GET'])
+def getProducts():
+    return jsonify({"products": products, "message": "Lista Completa"})
+
 if __name__ == '__main__':
     app.run(debug=True, port=4500)
