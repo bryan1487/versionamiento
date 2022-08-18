46b1a0380181167afcb21726a5ef95c0d5afb255
bryan1487 <bryan.solorzano.sep@gmail.com>
2022-08-18 00:29:31 -0400

    add get for a product

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ def ping():
 def getProducts():
     return jsonify({"products": products, "message": "Lista Completa"})
 
+@app.route('/products/<string:product_name>', methods=['GET'])
+def getProduct(product_name):
+    productFound = [product for product in products if product['name'] == product_name]
+
+    if (len(productFound)>0):
+        return jsonify({"product": productFound})
+    return jsonify({"message": "NOT FOUND"})
+
 if __name__ == '__main__':
     app.run(debug=True, port=4500)
