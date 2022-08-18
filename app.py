eb5f2d5624020b547e3604e85f576992450c0c48
bryan1487 <bryan.solorzano.sep@gmail.com>
2022-08-18 00:31:14 -0400

    add post to create product

--- app.py
+++ app.py
@@ -18,5 +18,16 @@ def getProduct(product_name):
         return jsonify({"product": productFound})
     return jsonify({"message": "NOT FOUND"})
 
+@app.route('/products', methods=['POST'])
+def createProduct():    
+    print(request.json)
+    new_product = {
+        "name": request.json['name'],
+        "price": request.json['price'],
+        "quantity": request.json['quantity']
+    }
+    products.append(new_product)
+    return jsonify({"message": "succes!!", "products": products})
+
 if __name__ == '__main__':
     app.run(debug=True, port=4500)
